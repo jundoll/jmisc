e1f3165f1074328ce07de188a4fb42de2b23f7d7
jundoll <jundoll33@gmail.com>
2015-11-23 16:27:11 +0900

    update "col" function

--- R/col.R
+++ R/col.R
@@ -1,6 +1,6 @@
-#' Easy color interpolation
+#' Easy color linear interpolation
 #' 
-#' @param x A vector of values corresponded to the colors
+#' @param x A vector (or array) of values corresponded to the colors
 #' @param colors A character vector of colors to interpolate
 #' 
 #' @return The interpolated color code
@@ -10,5 +10,13 @@
 #' plot(x, pch = 19, col = col(-x))
 #' @export
 col <- function(x, colors = c("blue", "red")) {
-  rgb( colorRamp(colors)(scale2(x, 0:1)) / 255 )
+  tmp_rgb <- colorRamp(colors)(scale2(x, 0:1))
+  rv_ind <- which(!is.na(tmp_rgb[, 1]))
+  res <- rep(NA_character_, length(x))
+  res[rv_ind] <- rgb(tmp_rgb[rv_ind, ], maxColorValue = 255)
+  if(is.array(x)) {
+    array(res, dim(x))
+  } else {
+    res
+  }
 }
